ce22a8414100ad38ef04c40eb5f93784d92eb024
vahkd33114 <vahkd33114@yahoo.co.jp>
2026-03-03 16:26:39 +0900

    [ update ] InputBarrage 機能追加しました

--- Assets/Scripts/InputBarrage.cs
+++ Assets/Scripts/InputBarrage.cs
@@ -2,19 +2,43 @@ using UnityEngine;
 
 public class InputBarrage : MonoBehaviour
 {
-    public int score = 0;
+    // クリック回数の初期値
+    private int _count = 0;
+
+    // 連打できるかどうか
+    private bool _canPress = false;
+    //private int _totalScore = 0;
+
+    void Start()
+    {
+        _canPress = false;
+    }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (_canPress && Input.GetMouseButtonDown(0))
         {
-            AddScore(10);
+            _count++;
         }
     }
 
-    void AddScore(int amount)
+    /// <summary>
+    /// スパチャタイムを開始・連打可能にする
+    /// </summary>
+    public void OnStart()
     {
-        score += amount;
-        Debug.Log($"{score}");
+        _canPress = true;
+        _count = 0;
+    }
+
+    /// <summary>
+    /// スパチャタイムを終了・連打不可能にする
+    /// スコアをScoreManagerに渡す
+    /// </summary>
+    public int OnEnd()
+    {
+        _canPress = false;
+        // ScoreManager.Instance.AddScore(_count);
+        return _count;
     }
 }
