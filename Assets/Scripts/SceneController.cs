a53f5bc431ca3499bfe21caba2a1b213a1fe544f
vahkd33114 <vahkd33114@yahoo.co.jp>
2026-03-04 16:33:12 +0900

    [ update ] SceneController FadeOut.csと統合・修正しました

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class SceneController : MonoBehaviour
 {
+    public Image fadePanel;
+    public float fadeDuracion = 1.0f;//フェードインまでにかかる時間
     public static SceneController instance {get; private set;}
 
     private void Awake()
@@ -17,7 +21,44 @@ public class SceneController : MonoBehaviour
         // シーン遷移しても破棄しないでね
         DontDestroyOnLoad(gameObject);
     }
+    
+    //フェードイン
+    private void Start()
+    {
+        fadePanel.enabled = false;
+    }
+    
+    public void OnClickFadeIn(string sceneName)
+    {
+        StartCoroutine(FadeOutAndLoadScene(sceneName));
+    }
 
+    private IEnumerator FadeOutAndLoadScene(string sceneName)
+    {
+        fadePanel.enabled = true;
+        
+        //経過時間
+        float elapsedTime = 0.0f;
+        
+        //フェードイン開始時の色（通常は透明）
+        Color startColor = fadePanel.color;
+        Color endColor = new Color(startColor.r, startColor.g, startColor.b, 1f);
+        while (elapsedTime < fadeDuracion)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Clamp01(elapsedTime / fadeDuracion);
+
+            fadePanel.color = Color.Lerp(startColor, endColor, alpha);
+
+            yield return null;
+        }
+
+        fadePanel.color = endColor;
+        
+        //フェードインが完了した後にシーンをロードするときはここで実行
+        LoadScene(sceneName); 
+    }
+    
     // シーン遷移用のメソッドですわよ🎮
     public void LoadScene(string sceneName)
     {
