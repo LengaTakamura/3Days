906c88f822a44fe5ca6b761bff4347158ec9f0a9
mayarei2662 <mayarei2662@gmail.com>
2026-05-14 09:46:36 +0900

    [Fix]クリック修正

--- Assets/Scripts/ClickDetector.cs
+++ Assets/Scripts/ClickDetector.cs
@@ -1,15 +1,41 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
+/// <summary>
+/// マウスクリックで画面遷移
+/// </summary>
 public class ClickDetector : MonoBehaviour
 {
+    [Header("シーンの名前")]
     [SerializeField] private string _sceneName;
+    [Header("フェードにかかる時間")]
+    [SerializeField] private float _fadeTime;
+    [Header("フェードパネル")]
+    [SerializeField] private Image _fade;
+
+    private float _currentTime = 0;
+
     private void Update()
     {
-        // 画面のどこかを押したら遷移する
         if (Input.GetMouseButtonDown(0))
         {
-            SceneController.instance.LoadScene(_sceneName);
-            Debug.Log("タイトルに遷移");
+            StartCoroutine(FadeOut());
+            Debug.Log("遷移完了");
         }
+    }
+
+    private IEnumerator FadeOut()
+    {
+        var c = _fade.color;
+        while (_currentTime < _fadeTime)
+        {
+            _currentTime += Time.deltaTime;
+            c.a = Mathf.Clamp01(_currentTime / _fadeTime) * 1;
+            _fade.color = c;
+
+            yield return null;
+        }
+        SceneController.instance.LoadScene(_sceneName);
     }
 }
